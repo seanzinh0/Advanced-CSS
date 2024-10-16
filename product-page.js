f7263c4e96f18d820cf0c130528d9c28f6aacef4
spichay <seanpichay1015@gmail.com>
2024-10-16 17:28:45 -0400

    Add event listeners to add functionality for product page buttons

---
+++ product-page.js
@@ -0,0 +1,44 @@
+const greBtn = document.getElementById("GRE-btn");
+const xtxBtn = document.getElementById("XTX-btn");
+const fourtyNinetyBtn = document.getElementById("4090-btn");
+const fourtySeventyBtn = document.getElementById("4070-btn");
+const tenEightyBtn = document.getElementById("1080Ti-btn");
+const seventySixHundredBtn = document.getElementById("7600XT-btn");
+const seventyEightHundredBtn = document.getElementById("7800XT-btn");
+const seventyNineHundredBtn = document.getElementById("7900XT-btn");
+
+greBtn.addEventListener("click", () => {
+	window.open(
+		"https://www.amazon.com/PowerColor-Devil-Radeon-GDDR6-Graphics/dp/B0CZBGW26S/ref=sr_1_1?crid=16D0UW5OD8US2&dib=eyJ2IjoiMSJ9.5BJQKWwWEiyhd6gYqKcdfJajlOj85cnB495vMduta6jJrz-rBZhSkcfyLvW5Z5gFjITrh7Dw9vPQ2mqUzjphB8o05X6nkgJAUms1j62bn_S4pG8LAioM0cfPR3FU9jF6gDREgu3mqysJCVzM7ekB9IOtWMdzIqVlfmD55URRq01hcMLDIA6ogXl_VHRxKDCzv-nEK9Peme_r_C5aXojDNqnIGrVMDMRpvc_YFM6w4mc.w1Uu2GDTDPNHPJKPlsJqYxh7VsydBoM7TudiYsyh2E8&dib_tag=se&keywords=powercolor%2Bred%2Bdevil%2Bradeon%2Brx%2B7900%2Bgre&qid=1729113206&sprefix=7900%2Bgre%2Bpowercolor%2Bred%2Caps%2C81&sr=8-1&th=1"
+	);
+});
+
+xtxBtn.addEventListener("click", () => {
+	window.open("https://www.amazon.com/ASRock-RX7900XTX-TC-24GO-RadeonTM/dp/B0BRYWVKCR");
+});
+
+fourtyNinetyBtn.addEventListener("click", () => {
+	window.open(
+		"https://www.amazon.com/NVIDIA-GeForce-Founders-Graphics-GDDR6X/dp/B0BJFRT43X/ref=sr_1_1?crid=192TLE93FQ5BB&dib=eyJ2IjoiMSJ9.KClYnknFFN1Xngc3nDVLlkbkD9alHvXbx-obFnIc5-ffQRRTqViUxy4BJFTuinT63rLbgE2n0gGAI6qIYTrVsE6H9Lz6YOtnqrycdYL3ZPjGyMfmz7Iy6HGTq3aT-a-hXR4o9jtpQCd0LeGRjsp0jBvJB79ffDwrJUv_1WhKYX8DKoybfgKzQuzQG01ITUsyRcu6gMHiow82jsXTaRa0RQqw1CiSLylMjuZmEtvVeGAYs5PjVCU55MpHTRpwQHETCmLPiFJZebqPGYLB_lx7SKbgZovqols2jkhI0W8gPoI.37uLwLqXlkBOSIPfxpk9CVj2dPE12fiMW-1nkkBYALs&dib_tag=se&keywords=rtx+4090+founders+edition&qid=1729113545&s=electronics&sprefix=rtx+4090+founder%2Celectronics%2C75&sr=1-1"
+	);
+});
+
+fourtySeventyBtn.addEventListener("click", () => {
+	window.open("https://www.amazon.com/MSI-RTX-4070-12G-Graphics/dp/B0CS7MBND4");
+});
+
+tenEightyBtn.addEventListener("click", () => {
+	window.open("https://www.amazon.com/EVGA-GeForce-Founders-Support-11G-P4-6390-KR/dp/B06XH2P8DD?th=1");
+});
+
+seventySixHundredBtn.addEventListener("click", () => {
+	window.open("https://www.amazon.com/ASRock-RX7600-SL-8GO-Graphics/dp/B0C6HTVV52");
+});
+
+seventyEightHundredBtn.addEventListener("click", () => {
+	window.open("https://www.amazon.com/ASRock-Radeon-RX7800XT-Graphics-Authorized-Dealer/dp/B0CHK2BNG8");
+});
+
+seventyNineHundredBtn.addEventListener("click", () => {
+	window.open("https://www.amazon.com/PowerColor-Radeon-7900-XT-Graphics-Card/dp/B0BMWHCGBZ");
+});
